2ab5718c28f07a8ec9a515841ff9b43b424f3c28
Aniket Das <64518145+anik8das@users.noreply.github.com>
2020-11-13 12:56:17 +0000

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-async function copyPageUrl() {
-    try {
-      yo = window.getSelection().toString();
-      await navigator.clipboard.writeText(location.href);
-      console.log('Page URL copied to clipboard');
-    } catch (err) {
-      console.error('Failed to copy: ', err);
-    }
-  }

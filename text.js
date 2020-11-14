6fda6f9102231fa24c9a1d669a683edf232e3224
Aniket Das <64518145+anik8das@users.noreply.github.com>
2020-11-14 02:28:57 +0000

    Delete text.js

--- text.js
+++
@@ -1,9 +0,0 @@
-
-chrome.commands.onCommand.addListener(function (command) {
-    if (command === "copy") {
-        chrome.tabs.query({'currentWindow':true,'active':true}, function(tabs){
-            chrome.tabs.sendMessage(tabs[0].id,'hi');
-        })
-        }
-    }
-);
